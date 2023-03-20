d5b1e9b103301177ec052da8c545f87c00772f19
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-03-20 14:31:12 +0530

    Update Hashset.java

--- Collection/Set/Hashset.java
+++ Collection/Set/Hashset.java
@@ -1,11 +1,19 @@
-import java.util.HashSet;
+ import java.util.HashSet;
   public class Vector {
       public static void main(String[] args) {
-         HashSet<String> names = new HashSet<String>();
+    	  HashSet<String> names = new HashSet<String>();
   		names.add("ram");
   		names.add("sam");
   		names.add("tom");
   		names.add("pin");
+  		names.add("sam");
   		System.out.println(names.size());
   		System.out.println(names);
   		System.out.println(names.contains("tom"));
+      }
+  }
+
+output:
+4
+[tom, pin, sam, ram]
+true
