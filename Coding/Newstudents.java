290efdaa675f4f543910c692c633df03efbd3e9d
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-03-23 14:19:27 +0530

    Create Newstudents.java

---
+++ Coding/Newstudents.java
@@ -0,0 +1,16 @@
+package Codetask;
+
+public class Newstudents {
+	private String nstudent;
+	public Newstudents(String ns)
+	{
+		this.nstudent=ns;
+	}
+	public String getNstudent() {
+		return nstudent;
+	}
+	public void setNstudent(String nstudent) {
+		this.nstudent = nstudent;
+	}
+	
+}
