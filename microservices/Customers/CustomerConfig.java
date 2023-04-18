801cdc1a5580f519c48c0bde8fb3ea3d8fa8d936
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-04-18 21:53:35 +0530

    Create CustomerConfig.java

---
+++ microservices/Customers/CustomerConfig.java
@@ -0,0 +1,25 @@
+package com.microservices.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+
+@Configuration
+public class CustomerConfig {
+	@Bean
+	public ModelMapper modelmapper() {
+		
+		return new ModelMapper();
+		
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+		
+	}
+		
+		
+
+}
