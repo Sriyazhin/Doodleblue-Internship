9a095f882cfeaa83558df72b0dddd5778289e11e
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-04-18 21:54:26 +0530

    Create Customers.java

---
+++ microservices/Customers/Customers.java
@@ -0,0 +1,62 @@
+package com.microservices.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="customers")
+public class Customers {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="contact")
+	private String contact;
+	
+	@Column(name="acctype")
+	private String accounType;
+	
+	@Column(name="ifsc")
+	private String ifsc;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getAccounType() {
+		return accounType;
+	}
+	public void setAccounType(String accounType) {
+		this.accounType = accounType;
+	}
+	public String getIfsc() {
+		return ifsc;
+	}
+	public void setIfsc(String ifsc) {
+		this.ifsc = ifsc;
+	}
+	
+	
+}
