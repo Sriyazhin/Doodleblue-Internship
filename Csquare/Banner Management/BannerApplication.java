23537ad9a7a7596193bf4d75961d5ec1c9ced9f2
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-06-12 18:07:01 +0530

    Create BannerApplication.java

---
+++ Csquare/Banner Management/BannerApplication.java
@@ -0,0 +1,13 @@
+package com.csquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsquareApplication.class, args);
+	}
+
+}
