e6f69115531e4ad4e4f18d4cf76bbd29c8adc98b
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-06-12 18:08:54 +0530

    Create ImageUpload.java

---
+++ Csquare/Banner Management/ImageUpload.java
@@ -0,0 +1,34 @@
+package com.csquare.BannerDet;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class ImageUpload {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int imageId;
+	private String imageType;
+
+	public int getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(int imageId) {
+		this.imageId = imageId;
+	}
+
+	public String getImageType() {
+		return imageType;
+	}
+
+	public void setImageType(String imageType) {
+		this.imageType = imageType;
+	}
+
+}
